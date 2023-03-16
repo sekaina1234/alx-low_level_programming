2f6c03996a32ab8d7ace6424b65298624748d5ea
sekaina1234 <soukainasaoud54@gmaill.com>
2023-03-16 02:34:05 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints exactly with proper grammar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
