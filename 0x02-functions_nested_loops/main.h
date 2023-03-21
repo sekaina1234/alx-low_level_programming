77797300b90320c2b076ce51752263fae0c5a275
sekaina1234 <soukainasaoud54@gmail.com>
2023-03-21 07:06:32 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-int _putchar(char c);
+#ifndef main_h
+#define main_h
+int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_×10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,3 +13,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+
+#endif
