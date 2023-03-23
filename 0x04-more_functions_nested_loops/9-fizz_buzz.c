619836091d782e225578af3c0c5c63a0009c3aed
sekaina1234 <soukainasaoud54@gmail.com>
2023-03-23 02:57:12 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,26 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
+{
 printf("FizzBuzz ");
+}
 else if (i % 3 == 0)
+{
 printf("Fizz ");
+}
 else if (i % 5 == 0)
+{
 printf("Buzz ");
+}
 else
+{
 printf("%d ", i);
+}
+if (x != 100)
+{
+printf(" ");
+}
 }
 printf("\n");
 return (0);
