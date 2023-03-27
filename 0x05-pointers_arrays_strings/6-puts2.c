91b46ff2f8d8cb4e83f5ac2819c55ad2bbf37394
sekaina1234 <soukainasaoud54@gmail.com>
2023-03-27 00:18:32 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
