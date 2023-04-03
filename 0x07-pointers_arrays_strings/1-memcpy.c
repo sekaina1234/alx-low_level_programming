1f514ed747e03f8fd269187abb2749988774e714
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-03 03:11:33 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Sccess)
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
