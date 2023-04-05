7a1a218fda4061240272729eaf9a25f4492beb2d
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-05 10:39:00 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,21 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * comparator - compares each character of the string.
- * @s: string
- * @n1: smallest interator.
- * @n2: biggest iterator.
- * Return: .
- */
-int comparator(char *s, int n1, int n2)
-{
-	if (*(s + n1) == *(s + n2))
-	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(0 + comparator(s, n1 + 1, n2 - 1)));
-	}
-	return (0);
-}
-
-/**
- * is_palindrome - detects if a string is a palindrome.
- * @s: string.
- * Return: 1 if s is a palindrome, 0 if not.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-if (*s == '\0')
+int len = strlen(s);
+if (len <= 1)
 return (1);
-return (comparator(s, 0, _strlen_recursion(s) - 1));
+else if (*s != *(s + len - 1))
+return (0);
+else
+{
+*(s + len - 1) = '\0';
+return (is_palindrome(s + 1));
+}
 }
