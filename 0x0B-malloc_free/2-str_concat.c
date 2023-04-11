f46590a206d5109706384ed6758271245fdfa32f
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-11 07:58:25 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,19 +10,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+int len1 = 0, len2 = 0, len3 = 0, len4 = 0;
 char *concat;
-size_t len1, len2;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-len1 = strlen(s1);
-len2 = strlen(s2);
-concat = malloc(len1 + len2 + 1);
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
+len4 = len1 + len2;
+concat = malloc((sizeof(char) * len4) +1);
 if (concat == NULL)
 return (NULL);
-memcpy(concat, s1, len1);
-memcpy(concat + len1, s2, len2);
-concat[len1 + len2] = '\0';
+len2 = 0;
+while (len3 <= len4)
+{
+if (len3 <= len1)
+concat[len3] = s1[len3];
+if (len3 >= len1)
+{
+concat[len3] = s2[len2];
+len2++;
+}
+len3++;
+}
+concat[len3] = '\0';
 return (concat);
 }
