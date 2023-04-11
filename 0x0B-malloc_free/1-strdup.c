283c1cb22c509a3932b0eb06febb1ca2565f15e4
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-11 07:16:26 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Duplicate a string
+ * @str: the string to duplicate
+ *
+ * Return: the strig duplicated
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int i, len;
+if (str == NULL)
+return (NULL);
+len = 0;
+while (str[len])
+len++;
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+dup[i] = str[i];
+return (dup);
+}
