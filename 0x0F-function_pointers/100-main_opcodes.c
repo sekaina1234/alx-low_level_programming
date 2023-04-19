b9b58a23d3fa109b996919a9773c44c7093f91c7
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-19 15:12:51 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print_opcodes(char *addr, int len);
 /**
- * main - Entry point
- * @argc: the number of parameters.
- * @argv: the parameters in the case the number of bytes.
+ * main - prints the opcodes of its own main function
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments passed to the program
  *
- * Return: 0 in success
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-int num, j;
+int num_bytes;
+char *addr = (char *)main;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-num = atoi(argv[1]);
-if (num < 0)
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
 {
 printf("Error\n");
-exit(2);
+return (2);
 }
-for (j = 0; j < num; j++)
-{
-printf("%02hhx\n ", *((char *)main + j));
-if (j < num - 1)
-printf(" ");
-else
-printf("\n");
-}
+print_opcodes(addr, num_bytes);
 return (0);
+}
+/**
+ * print_opcodes - prints the opcodes of a given address
+ * @addr: the starting address to print from
+ * @len: the number of bytes to print
+ */
+void print_opcodes(char *addr, int len)
+{
+int i;
+for (i = 0; i < len; i++)
+printf("%02hhx ", *(addr + i));
+printf("\n");
 }
