50db1cea48821f4679dfbf1cefaa3b31110f064c
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-19 06:11:48 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,22 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-char *op;
+int (*op)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 return (98);
 }
-num1 = atoi(argv[1]);
-op = argv[2];
-num2 = atoi(argv[3]);
-if ((*op == '/' || *op == '%') && num2 == 0)
+op = get_op_func(argv[2]);
+if (!op)
 {
 printf("Error\n");
-return (100);
+return (99);
 }
-result = (*get_op_func(op))(num1, num2);
-printf("%d\n", result);
+printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }
