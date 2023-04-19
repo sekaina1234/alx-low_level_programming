4c7e92320eee876c21d6c55d8438cc6738b6358b
sekaina1234 <soukainasaoud54@gmail.com>
2023-04-19 04:56:22 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,31 +11,3 @@ void print_name(char *name, void (*f)(char *))
 {
 f(name);
 }
-void print_name_as_is(char *name)
-{
-printf("Hello, my name is %s\n", name);
-}
-void print_name_uppercase(char *name)
-{
-unsigned int i = 0;
-printf("Hello, my uppercase name is ");
-while (name[i])
-{
-if (name[i] >= 'a' && name[i] <= 'z')
-{
-putchar(name[i] + 'A' - 'a');
-}
-else
-{
-putchar(name[i]);
-}
-i++;
-}
-putchar('\n');
-}
-int main(void)
-{
-print_name("Bob", print_name_as_is);
-print_name("Bob Dylan", print_name_uppercase);
-return (0);
-}
