5ffd7e5ce57599860e70d302cc390fd57bfbfcc8
sekaina1234 <soukainasaoud54@gmail.com>
2023-05-01 16:21:09 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,11 @@ int pop_listint(listint_t **head)
 {
 listint_t *t;
 int s;
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 return (0);
 t = *head;
+s = t->n;
 *head = (*head)->next;
-n = t->s;
 free(t);
 return (s);
 }
