a02b8c392a2f65ee252a5e93be0b2153ab6924f0
sekaina1234 <soukainasaoud54@gmail.com>
2023-05-04 05:03:53 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit - ...
+ * @n: ...
+ * @index: ...
+ * Return: ...
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+*n &= ~(1 << index);
+return (1);
+}
