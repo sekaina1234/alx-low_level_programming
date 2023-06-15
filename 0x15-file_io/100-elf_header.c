d4d7f73a017b241aece76a2623abc24f0d64f90d
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-15 16:52:46 -0700

    10. Palindromes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,62 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-/**
- * error - ...
- * @msg:...
- */
-void error(char *msg)
-{
-fprintf(stderr, "%s\n", msg);
-exit(98);
-}
-/**
- * print_elf_header - ...
- * @header: ...
- */
-void print_elf_header(Elf64_Ehdr *header)
-{
-int i;
-printf("ELF Header:\n");
-printf("  Magic:   ");
-for (i = 0; i < EI_NIDENT; i++)
-{
-printf("%02x%c", header->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
-printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Unknown");
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-printf("  Type:                              %s\n", header->e_type == ET_NONE ? "NONE (Unknown type)" :
-header->e_type == ET_REL ? "REL (Relocatable file)" :
-header->e_type == ET_EXEC ? "EXEC (Executable file)" :
-header->e_type == ET_DYN ? "DYN (Shared object file)" :
-header->e_type == ET_CORE ? "CORE (Core file)" :
-"Unknown type");
-printf("  Entry point address:               0x%lx\n", header->e_entry);
-}
-}
-/**
- * main - ...
- * @argc: ...
- * @argv: ...
- * Return: ...
- */
+#include <string.h>
+#define ELF_MAGIC "\x7f""ELF"
+#define EI_NIDENT 16
+#define EI_CLASS 4
+#define ELFCLASSNONE 0
+#define ELFCLASS32 1
+#define ELFCLASS64 2
+#define EI_DATA 5
+#define ELFDATANONE 0
+#define ELFDATA2LSB 1
+#define ELFDATA2MSB 2
+#define EI_VERSION 6
+#define EV_CURRENT 1
+#define EI_OSABI 7
+#define ELFOSABI_NONE 0
+#define ELFOSABI_HPUX 1
+#define ELFOSABI_NETBSD 2
+#define ELFOSABI_LINUX 3
+#define ELFOSABI_SOLARIS 6
+#define ELFOSABI_FREEBSD 9
+#define ELFOSABI_ARM 97
+#define EI_ABIVERSION 8
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
 int main(int argc, char **argv)
 {
-int fd;
-if (argc != 2)
+if (argc < 2)
 {
-fprintf(stderr, "Usage: %s <ELF binary>\n", argv[0]);
-return (EXIT_FAILURE);
+fprintf(stderr, "Usage: %s <elf_file>\n", argv[0]);
+exit(1);
 }
-fd = open(argv[1], O_RDONLY);
-if (fd < 0)
+FILE *file = fopen(argv[1], "rb");
+if (file == NULL)
 {
-perror("open");
-return (EXIT_FAILURE);
+perror("Error opening file");
+exit(1);
 }
-return (EXIT_SUCCESS);
+unsigned char e_ident[EI_NIDENT];
+if (fread(e_ident, sizeof(e_ident), 1, file) != 1)
+{
+perror("Error reading ELF header");
+exit(1);
 }
+print_magic(e_ident);
+print_class(e_ident);
+print_data(e_ident);
+print_version(e_ident);
+print_osabi(e_ident);
+unsigned int e_type;
+if (fseek(file, EI_CLASS == ELFCLASS32 ? 16 : 24, SEEK_SET) != 0 ||
+fread(&e_type, sizeof(e_type), 1, file) != 1)
+{
+perror("Error reading ELF header");
+exit(1);
+}
+print_type(e_type, e_ident);
+fclose(file);
+return (0);
+}
+void print_magic(unsigned char *e_ident)
+{
+if (memcmp(e_ident, ELF_MAGIC, 4) == 0)
+{
+printf("ELF Header: ");
+for (int i = 0; i < EI_NIDENT; i++)
+{
+printf("%02x ", e_ident[i]);
+}
+printf("\n");
+}
+else
+{
+fprintf(stderr, "Error: Not an ELF file\n");
+exit(1);
+}
+}
+void print_class(unsigned char *e_ident)
+{
+printf("  Class: ");
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+printf("none\n");
+break;
+case ELFCLASS32:
+printf("ELF32\n");
+break;
+case ELFCLASS64:
+printf("ELF64\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+}
+}
+void print_data(unsigned char *e_ident)
+{
+printf("  Data: ");
+switch (e_ident[EI_DATA])
+{
+case ELFDATANONE:
+printf("none\n");
+break;
+case ELFDATA2LSB
