504040d87c4399e5ee0756b0a314af958b8b9c44
sekaina1234 <soukainasaoud54@gmail.com>
2023-06-19 15:24:40 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,47 +2,61 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node
- * at a given position in a doubly linked list.
- * @h: A pointer to a pointer to the head of the list.
- * @idx: The index at which the new node should be inserted.
- * @n: The value to be stored in the new node.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to the head of the linked list.
+ * @idx: Index of the list where the new node should be added.
+ * @n: Value to be stored in the new node.
  *
- * Return: The address of the new node, or NULL if it failed.
+ * Return: Address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *current = *h;
-unsigned int count = 0;
-if (idx == 0)
-{
+dlistint_t *new_node, *current;
+unsigned int i = 0;
+
+if (h == NULL)
+return (NULL);
+
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
+
 new_node->n = n;
 new_node->prev = NULL;
-new_node->next = *h;
-if (*h != NULL)
-(*h)->prev = new_node;
+new_node->next = NULL;
+
+if (*h == NULL)
+{
 *h = new_node;
 return (new_node);
 }
+
+current = *h;
 while (current != NULL)
 {
-if (count == idx)
+if (i == idx)
 {
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-new_node->prev = current->prev;
 new_node->next = current;
+new_node->prev = current->prev;
+if (current->prev != NULL)
+current->prev->next = new_node;
+current->prev = new_node;
+if (i == 0)
+*h = new_node;
+return (new_node);
+}
+current = current->next;
+i++;
+}
+
+if (i == idx)
+{
+new_node->prev = current->prev;
 current->prev->next = new_node;
 current->prev = new_node;
 return (new_node);
 }
-count++;
-current = current->next;
-}
+
+free(new_node);
 return (NULL);
 }
