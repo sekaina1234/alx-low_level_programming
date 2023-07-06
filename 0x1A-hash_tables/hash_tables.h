3c3f941c8aa76871a1088982b9e942582343387c
sekaina123 <soukainasaoud54@gmail.com>
2023-07-06 14:14:48 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,22 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key (string)
+ * @value: The value corresponding to the key
+ * @next: A pointer to the next node
+ */
 typedef struct hash_node_s
 {
 char *key;
 char *value;
 struct hash_node_s *next;
 } hash_node_t;
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: An array of pointers to hash nodes
+ */
 typedef struct hash_table_s
 {
 unsigned long int size;
