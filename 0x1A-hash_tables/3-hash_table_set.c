4ce7ef88ee87e3ed755a459df76fc5be88f99bd3
sekaina123 <soukainasaoud54@gmail.com>
2023-07-07 03:59:35 -0700

    3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - Adds or updates a key/value pair in the hash table.
+ * @ht: The hash table.
+ * @key: The key string.
+ * @value: The value string.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int index;
+hash_node_t *new_node = NULL, *tmp = NULL;
+
+if (ht == NULL || key == NULL || *key == '\0')
+return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
+
+while (tmp)
+{
+if (strcmp(tmp->key, key) == 0)
+{
+free(tmp->value);
+tmp->value = strdup(value);
+return (1);
+}
+tmp = tmp->next;
+}
+
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+return (0);
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+
+return (1);
+}
