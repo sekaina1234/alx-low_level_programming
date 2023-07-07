3a860e4d421c9d4adc18e70bab1dc1ff490a321a
sekaina123 <soukainasaoud54@gmail.com>
2023-07-07 04:11:39 -0700

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+int flag = 0;
+hash_node_t *node;
+
+if (ht == NULL)
+return;
+
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node)
+{
+if (flag == 1)
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);
+flag = 1;
+node = node->next;
+}
+}
+printf("}\n");
+}
